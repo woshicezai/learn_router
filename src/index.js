95fb1f3189748e4de770c813233b8c84646081f2
haokairui <haokairui@hualala.com>
2019-11-22 07:11:31 +0800

    x

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import {
   useRouteMatch
 } from "react-router-dom";
 
-import {Topics} from "./Topics";
-
 function NestingExample() {
   return (
     <Router>
@@ -54,7 +52,52 @@ class Home extends React.Component {
   }
 }
 
+function Topics() {
+  // The `path` lets us build <Route> paths that are
+  // relative to the parent route, while the `url` lets
+  // us build relative links.
+  let {path, url} = useRouteMatch();
+  console.log("Topics render", path, url);
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${path}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${path}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${path}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
 
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic/>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let {topicId} = useParams();
+  console.log("Topic render");
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
 
 ReactDom.render(
   <NestingExample/>,
